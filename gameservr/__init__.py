95b8316a98ef68571ec047183cacd1965541eb43
fillnye <60429866+fillnye@users.noreply.github.com>
2025-02-22 14:37:48 -0500

    did stuff

--- gameservr/__init__.py
+++ gameservr/__init__.py
@@ -3,29 +3,53 @@ import os
 from flask import render_template
 from flask import Flask
 from enum import Enum
+import time
 
 class Status(Enum):
     STARTING = 0
     INROUND = 1
-    INTERROUND = 3
-    FAULT = 4
+    INTERROUND = 2
+    FAULT = 3
+
+class User:
+    def __init__(self, users, age):
+        self.name = name
+        self.id = age
+        self.points = 0
 
 
-class room:
+class Room:
     def __init__(self, users, age):
         self.users = users
         self.age = age
         self.challengecode = "0"
-        self.status = Status.INROUND
-    def startRound():
+        self.round = 0
+        self.status = Status.STARTING
+
+    def mainLoop(self):
+        while(self.status!=FAULT):
+            if(self.status==STARTING):
+                startRound(self)
+            elif(self.status==INTERROUND):
+                startRound(self)
+            
+    def startRound(self):
         if(len(self.users)<1):
+            print("NOT ENOUGHT USERS TO START")
+            time.sleep(40)
             return
         self.challengecode = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-        print()
+        printcode()
         self.status = Status.INROUND
-    def print():
+    def printcode(self):
         print(self.challengecode)
-    def challenge()
+    def challenge(self,challengecode,user):
+        if(challengecode==self.challengecode):
+            self.status = Status.INTERROUND
+            user.points+=10
+            return True
+        else:
+            return False
 
 
 def create_app(test_config=None):
@@ -35,7 +59,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/index.html')
     def index():
-        return render_template("index.html");
+        return render_template("index.html")
 
     @app.route('/user/register.html')
     def reg():
@@ -43,28 +67,29 @@ def create_app(test_config=None):
             #Proccses reg data
             return "Succses"
         
-        return render_template("user/register.html");
+        return render_template("user/register.html")
 
     @app.route('/user/view.html')
     def usrview():
-        return render_template("user/view.html");
+        return render_template("user/view.html")
 
     @app.route('/room/join.html')
     def joinroom():
-        return render_template("user/edit.html");
+        return render_template("user/edit.html")
 
     @app.route('/room/<int:roomid>')
     def room(roomid):
-        return render_template("user/edit.html");
+        return render_template("user/edit.html")
 
     @app.route('/room/<int:roomid>/status')
-    def room(roomid):
+    def roomstat(roomid):
         return 
 
     @app.route('/room/<int:roomid/challenge>')
     def roomchlg(roomid):
         if request.method == "POST":
-        
+            return
+        return
 
     app.add_url_rule("/", endpoint="index")
     return app
